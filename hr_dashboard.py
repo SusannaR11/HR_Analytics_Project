4e047e4fd45e41583e7db1e9244d365c9323fec8
Richard <norrmanrichard@gmail.com>
2025-05-06 16:11:02 +0200

    added a connection to the data warehouse

--- hr_dashboard.py
+++ hr_dashboard.py
@@ -1,2 +1,8 @@
-# Streamlit dashboard for visualising and analysing HR metrics
+import streamlit as st
+import duckdb
+from pathlib import Path
+from streamlit_option_menu import option_menu
 
+# Connecting to the data warehouse
+db_path = Path(__file__).parent / "ads_data_warehouse.duckdb"
+connection = duckdb.connect(database=str(db_path), read_only=True)
