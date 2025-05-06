b51c6fd5ddeb05c528e5241477ab8ddd7b88dd27
Richard <norrmanrichard@gmail.com>
2025-05-06 16:32:30 +0200

    remodified and styled the streamlit dashboard sidebar

--- hr_dashboard.py
+++ hr_dashboard.py
@@ -6,3 +6,22 @@ from streamlit_option_menu import option_menu
 # Connecting to the data warehouse
 db_path = Path(__file__).parent / "ads_data_warehouse.duckdb"
 connection = duckdb.connect(database=str(db_path), read_only=True)
+
+# Sidebar for different options
+with st.sidebar:
+    selected = option_menu(
+        menu_title=None,
+        options=["Home", "Säkerhet och bevakning", "Yrken med social inriktning", "Data/IT"],
+        icons=["house", "fingerprint", "person-arms-up", "pc-display-horizontal"],
+        default_index=0
+    )
+
+# Changing "page" based on selected option
+if selected == "Home":
+    st.title (f"{selected}")
+if selected == "Säkerhet och bevakning":
+    st.title (f"{selected}")
+if selected == "Yrken med social inriktning":
+    st.title (f"{selected}")
+if selected == "Data/IT":
+    st.title (f"{selected}")
