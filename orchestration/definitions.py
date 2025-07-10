eea0c73128b8ff9dca9924b8fb4ead7a93bf139f
Susanna Rokka <susannaajrokka@gmail.com>
2025-07-10 18:57:38 +0200

    Adjusting decorators for sensors

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -8,16 +8,15 @@ from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
 from dagster_dlt import DagsterDltResource, dlt_assets
 from pathlib import Path
 import dlt
-from load_api import jobads_source_data_it, jobads_source_sakerhet_bevakning, jobads_source_socialt_arbete
+from load_api import (jobads_source_data_it, jobads_source_sakerhet_bevakning, jobads_source_socialt_arbete)
 
 # import Dagsater wrapper source
 import sys
-sys.path.insert(0, str(Path(__file__).parents[0])) #points at project root
-from load_api import jobads_source
+sys.path.insert(0, str(Path(__file__).parents[1])) #points at project root
 
 # Paths to duckdb warehouse and dbt
 db_path = str(Path(__file__).parents[1] / "ads_data_warehouse.duckdb")
-dbt_project_directory = Path(__file__).parent / "dbt_code"
+dbt_project_directory = Path(__file__).parents[1] / "dbt_code"
 profiles_dir = Path.home() / ".dbt"
 
 # dlt resource
@@ -26,6 +25,7 @@ dbt_resource = DbtCliResource(project_dir=dbt_project_directory, profiles_dir=pr
 
 
 # dlt assets for 3 occupation fields
+# Data / IT dlt Asset
 @dlt_assets(
     dlt_source=jobads_source_data_it(),
     dlt_pipeline=dlt.pipeline(
@@ -37,6 +37,7 @@ dbt_resource = DbtCliResource(project_dir=dbt_project_directory, profiles_dir=pr
 def dlt_load_data_it(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
     yield from dlt.run(context=context)
 
+# Socialt arbete dlt Asset
 @dlt_assets(
     dlt_source=jobads_source_socialt_arbete(),
     dlt_pipeline=dlt.pipeline(
@@ -48,6 +49,7 @@ def dlt_load_data_it(context: dg.AssetExecutionContext, dlt: DagsterDltResource)
 def dlt_load_social(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
     yield from dlt.run(context=context)
 
+# Säkerhet och bevakning dlt Asset
 @dlt_assets(
     dlt_source=jobads_source_sakerhet_bevakning(),
     dlt_pipeline=dlt.pipeline(
@@ -58,25 +60,76 @@ def dlt_load_social(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
 )
 def dlt_load_security(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
     yield from dlt.run(context=context)
-    
-# dbt resource
-dbt_project = DbtProject(project_dir=dbt_project_directory, profiles_dir=profiles_dir)
-dbt_project.prepare_if_dev()
 
+# dbt Asset
+dbt_project = DbtProject(project_dir=dbt_project_directory, profiles_dir=profiles_dir)
+#dbt_project.prepare_if_dev()
+
+# Asset for running 'dbt build'
 @dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
 # Jobs
-job_dlt = dg.define_asset_job("job_dlt", selection=dg.AssetSelection.keys("dlt_jobads_source_jobsearch_resource"))
-job_dbt = dg.define_asset_job("job_dbt", selection=dg.AssetSelection.keys("occupation_socialt_arbete"))
-
-# Schedule
-schedule_dlt = dg.ScheduleDefinition(
-    job=job_dlt,
-    cron_schedule="9 00 * * *" 
+job_api = dg.define_asset_job(
+    "job_api",
+    selection=dg.AssetSelection.keys(
+        "dlt_jobads_source_data_it_jobsearch_resource",
+        "dlt_jobads_source_social_jobsearch_resource",
+        "dlt_jobads_source_security_jobsearch_resource",
+    )
 )
 
+job_dbt = dg.define_asset_job(
+    "job_dbt", 
+    selection=dg.AssetSelection.keys(
+        "occupation_data_it",
+        "occupation_sakerhet_bevakning",
+        "occupation_socialt_arbete"
+        )
+)
+
+job_track = dg.define_asset_job(
+    "job_track",
+    selection=dg.AssetSelection.keys(
+        "dlt_jobads_source_data_it_jobsearch_resource",
+        "dlt_jobads_source_social_jobsearch_resource",
+        "dlt_jobads_source_security_jobsearch_resource",
+    )
+)
+
+# Schedule
+# API path refresh daily
+schedule_api = dg.ScheduleDefinition(
+    job=job_api,
+    cron_schedule="7 00 * * *"  #Daily at 7:00 UTC ie 9:00 ECT
+)
+
+# Sensors
+# if API refreshes succesfully, sensors will trigger to dbt build and 
+# monitor each occupation field daily for new jobs
+
+@dg.run_status_sensor(run_status=dg.DagsterRunStatus.SUCCESS, monitored_jobs=[job_api], name="api_success_sensor")
+def api_success_sensor(context):
+    yield dg.RunRequest(job_name="job_dbt")
+
+@dg.run_status_sensor(run_status=dg.DagsterRunStatus.SUCCESS, monitored_jobs=[job_dbt], name="dbt_success_sensor")
+def dbt_success_sensor(context):
+    yield dg.RunRequest(job_name="job_track")
+
+#Definitions 
+
+defs = dg.Definitions(
+    assets=[dlt_load_data_it, dlt_load_social, dlt_load_security, dbt_models],
+    resources={"dlt": dlt_resource, "dbt": dbt_resource},
+    jobs=[job_api, job_dbt, job_track],
+    schedules=[schedule_api],
+    sensors=[api_success_sensor, dbt_success_sensor],
+)
+
+
+# Deploy Dagster UI using command:
+# dagster dev -f orchestration/definitions.py from project root folder
 
 
 
